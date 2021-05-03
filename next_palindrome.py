a5145e21a8138e7fa1c63db3201c37fedb84c3f8
Cory Cluff <ccluff@clearwateranalytics.com>
2021-05-03 14:35:54 -0600

    reordering

--- next_palindrome.py
+++ next_palindrome.py
@@ -12,11 +12,11 @@ def next_palindrome(num: int):
     half_len = ceil(len(start) / 2)
     half_string = start[:half_len]
     new_half_string = str(int(half_string) + 1)
-    need_new_digit = len(new_half_string) > len(half_string)
 
     left_len = half_len - 1 if len(start) % 2 else half_len
     left = new_half_string[:left_len]
 
+    need_new_digit = len(new_half_string) > len(half_string)
     right_len = half_len + 1 if need_new_digit else half_len
     right = new_half_string[:right_len][::-1]
 
